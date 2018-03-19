5926601f05ea2e9db2e0abf7b042b6809ef52c86
LunchboxMG <mikey53x@yahoo.com>
2018-03-19 04:45:40 -0500

    GL Updates.

--- jlunchbox/src/com/jlunchbox/core/graphics/gl/UniformMatrix.java
+++ jlunchbox/src/com/jlunchbox/core/graphics/gl/UniformMatrix.java
@@ -1,5 +1,23 @@
 package com.jlunchbox.core.graphics.gl;
 
-public class UniformMatrix {
+import java.nio.FloatBuffer;
+
+import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL20;
+
+public class UniformMatrix extends Uniform{
+	
+	// TODO: Look into using lwjgl memory
+	private FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
+	
+	public UniformMatrix(String name) {
+		super(name);
+	}
+	
+	public void loadMatrix(Matrix4f matrix){
+		matrix.get(buffer);
+		GL20.glUniformMatrix4fv(super.getLocation(), false, buffer);
+	}
 
 }
