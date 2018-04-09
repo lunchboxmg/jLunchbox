ae20ea0d71627a69cf0bd9f01e8e5a7a84128442
LunchboxMG <mikey53x@yahoo.com>
2018-04-09 13:50:49 -0500

    Camera update

--- jlunchbox/src/com/jlunchbox/core/graphics/Camera.java
+++ jlunchbox/src/com/jlunchbox/core/graphics/Camera.java
@@ -1,9 +1,44 @@
 package com.jlunchbox.core.graphics;
 
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
 public class Camera {
+	
+	private static final Vector3f WORLD_UP = new Vector3f(0, 1, 1);
+	
+	private Vector3f position;
+	private Vector3f target;
+	private Matrix4f view;
 
 	public Camera() {
-		// TODO Auto-generated constructor stub
+		this(new Vector3f(0, 0, 0));
 	}
+	
+	public Camera(Vector3f target) {
+		this.target = target;
+		create();
+	}
+	
+	private void create() {
+		this.position = new Vector3f();
+		this.view = new Matrix4f();
+		update();
+	}
+	
+	public void update() {
+		
+		// Constrain parameters
+		// Calculate new position
+		// Update view matrix
+		view.lookAt(position, target, WORLD_UP, view);
+		
+	}
+	
+	// GETTERS
+	
+	public Vector3f getPosition() { return position; }
+	public Vector3f getTarget() { return target; }
+	public Matrix4f getView() { return view; }
 
 }
